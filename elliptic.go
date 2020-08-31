688785bd0b5484692f22281f88013837ca829986
elizarpif <liza.pif@mail.ru>
2020-08-31 17:38:25 +0300

    added elliptic curve in finite field

--- elliptic.go
+++ elliptic.go
@@ -3,13 +3,134 @@ package ellipticcrypto
 import (
 	"fmt"
 	"math"
+	"math/big"
+	"reflect"
 )
 
+// точка на кривой
+type pointField struct {
+	X *big.Int
+	Y *big.Int
+}
+
+func (p pointField) String() string {
+	return fmt.Sprintf("(%d, %d)", p.X.Int64(), p.Y.Int64())
+}
+
+// эллиптическая кривая над полем порядка p
+type ellipticCurveField struct {
+	// y^2 = x^3 +ax + b
+	a, b *big.Int
+	// mod p
+	p *big.Int
+}
+
+// реализация скалярного умножения nP = P+P+..+P
+func (ec ellipticCurveField) Mul(p1 pointField, n int) pointField {
+	p2 := p1
+	for i := 1; i < n; i++ {
+		// fmt.Println(p2)
+		p2 = ec.Sum(p1, p2)
+	}
+	// fmt.Println(p2)
+	return p2
+}
+
+// реализация скалярного сложения P1+P2
+func (ec ellipticCurveField) Sum(p1, p2 pointField) pointField {
+	if reflect.DeepEqual(p1, p2) {
+		return ec.Double(p1)
+	}
+
+	o := pointField{
+		X: new(big.Int),
+		Y: new(big.Int),
+	}
+
+	if reflect.DeepEqual(p1, o) {
+		return p2
+	}
+	if reflect.DeepEqual(o, p2) {
+		return p1
+	}
+
+	dy := new(big.Int)
+	dy = big.NewInt(p1.Y.Int64() - p2.Y.Int64())
+
+	dx := &big.Int{}
+	dx = big.NewInt(p1.X.Int64() - p2.X.Int64())
+
+	if dx.Int64() == 0 {
+		return pointField{
+			X: big.NewInt(0),
+			Y: big.NewInt(0),
+		}
+	}
+
+	m := new(big.Int)
+	tmp := new(big.Int)
+	m.Mul(dy, tmp.ModInverse(dx, ec.p))
+	m.Mod(m, ec.p)
+
+	mInt := m.Int64()
+
+	xRes := big.NewInt(mInt*mInt - p1.X.Int64() - p2.X.Int64())
+	xRes = xRes.Mod(xRes, ec.p)
+
+	yRes := big.NewInt(p2.Y.Int64() + mInt*(xRes.Int64()-p2.X.Int64()))
+	yRes.Neg(yRes).Mod(yRes, ec.p)
+
+	return pointField{
+		X: xRes,
+		Y: yRes,
+	}
+}
+
+// реализация удвоения точки 2P=P+P
+func (ec ellipticCurveField) Double(p1 pointField) pointField {
+	x1 := p1.X.Int64()
+
+	dy := x1*x1*3 + ec.a.Int64()
+
+	dyModP := new(big.Int)
+	dyModP.Mod(big.NewInt(dy), ec.p)
+
+	dx := 2 * p1.Y.Int64()
+
+	dxModP := new(big.Int)
+	dxModP.Mod(big.NewInt(dx), ec.p)
+
+	m := new(big.Int)
+	m.Mul(dyModP, (&big.Int{}).ModInverse(dxModP, ec.p))
+	m.Mod(m, ec.p)
+
+	mInt := m.Int64()
+
+	xRes := big.NewInt(mInt*mInt - 2*x1)
+	xRes = xRes.Mod(xRes, ec.p)
+
+	yRes := big.NewInt(m.Int64()*(x1-xRes.Int64()) - p1.Y.Int64())
+	yRes = yRes.Mod(yRes, ec.p)
+
+	return pointField{
+		X: xRes,
+		Y: yRes,
+	}
+}
+
+// эллиптическая кривая в R2
 type ellipticCurve struct {
 	// y^2 = x^3 +ax + b
 	a, b float64
 }
 
+// нахождение y зная x
+func (ec ellipticCurve) Y(x float64) float64 {
+	res := math.Sqrt(x*x*x + ec.a*x + ec.b)
+	return res
+}
+
+// некая точка
 type point struct {
 	X float64
 	Y float64
@@ -19,6 +140,7 @@ func (p point) String() string {
 	return fmt.Sprintf("(%.5f,%.5f)", p.X, p.Y)
 }
 
+// реализация сложения двух точек на эллиптической кривой
 func (ec ellipticCurve) SumPoints(p1 point, p2 point) point {
 	if p1.Y == 0 {
 		return p2
--- elliptic_test.go
+++ elliptic_test.go
@@ -1,43 +1,279 @@
 package ellipticcrypto
 
 import (
+	"math/big"
+	"reflect"
 	"testing"
 )
 
 func TestEllipticCurve_SumPoints(t *testing.T) {
 	tests := []struct {
-		name string
+		name          string
 		P             point
 		Q             point
 		curve         ellipticCurve
 		expectedPoint point
 	}{
 		{
-			name: "simple sum",
+			name:  "simple sum",
 			curve: ellipticCurve{a: -36},
-			P: point{-3, 9},
-			Q: point{-2, 8},
+			P:     point{-3, 9},
+			Q:     point{-2, 8},
 			expectedPoint: point{
 				X: 6,
 				Y: 0,
 			},
 		},
 		{
-			name: "2P",
+			name:  "2P",
 			curve: ellipticCurve{a: -36},
-			P: point{-3, 9},
-			Q: point{-3, 9},
+			P:     point{-3, 9},
+			Q:     point{-3, 9},
 			expectedPoint: point{
-				X: float64(25)/float64(4),
-				Y: -float64(35)/float64(8),
+				X: float64(25) / float64(4),
+				Y: -float64(35) / float64(8),
 			},
 		},
 	}
-	for _, tt := range tests{
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			res := tt.curve.SumPoints(tt.P, tt.Q)
-			if res != tt.expectedPoint{
+			if res != tt.expectedPoint {
 				t.Fatalf("not expected result %s != %s", res, tt.expectedPoint)
+			}
+		})
+	}
+}
+
+func Test_ellipticCurveField_Double(t *testing.T) {
+	type fields struct {
+		a *big.Int
+		b *big.Int
+		p *big.Int
+	}
+	type args struct {
+		p1 pointField
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   pointField
+	}{
+		{
+			name: "test double",
+			fields: fields{
+				a: big.NewInt(2),
+				b: big.NewInt(3),
+				p: big.NewInt(97),
+			},
+			args: args{
+				p1: pointField{
+					X: big.NewInt(3),
+					Y: big.NewInt(6),
+				},
+			},
+			want: pointField{
+				X: big.NewInt(80),
+				Y: big.NewInt(10),
+			},
+		},
+		{
+			name: "test double",
+			fields: fields{
+				a: big.NewInt(2),
+				b: big.NewInt(3),
+				p: big.NewInt(97),
+			},
+			args: args{
+				p1: pointField{
+					X: big.NewInt(3),
+					Y: big.NewInt(91),
+				},
+			},
+			want: pointField{
+				X: big.NewInt(0),
+				Y: big.NewInt(0),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ec := ellipticCurveField{
+				a: tt.fields.a,
+				b: tt.fields.b,
+				p: tt.fields.p,
+			}
+			if got := ec.Double(tt.args.p1); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Double() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_ellipticCurveField_Sum(t *testing.T) {
+	type fields struct {
+		a *big.Int
+		b *big.Int
+		p *big.Int
+	}
+	type args struct {
+		p1 pointField
+		p2 pointField
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   pointField
+	}{
+		{
+			name: "check sum with equal x coordinates",
+			fields: fields{
+				a: big.NewInt(2),
+				b: big.NewInt(3),
+				p: big.NewInt(97),
+			},
+			args: args{
+				p1: pointField{
+					X: big.NewInt(3),
+					Y: big.NewInt(91),
+				},
+				p2: pointField{
+					X: big.NewInt(3),
+					Y: big.NewInt(6),
+				},
+			},
+			want: pointField{
+				X: big.NewInt(0),
+				Y: big.NewInt(0),
+			},
+		},
+		{
+			name: "check simple sum",
+			fields: fields{
+				a: big.NewInt(2),
+				b: big.NewInt(3),
+				p: big.NewInt(97),
+			},
+			args: args{
+				p1: pointField{
+					X: big.NewInt(3),
+					Y: big.NewInt(6),
+				},
+				p2: pointField{
+					X: big.NewInt(80),
+					Y: big.NewInt(10),
+				},
+			},
+			want: pointField{
+				X: big.NewInt(80),
+				Y: big.NewInt(87),
+			},
+		},
+		{
+			name: "check double",
+			fields: fields{
+				a: big.NewInt(2),
+				b: big.NewInt(3),
+				p: big.NewInt(97),
+			},
+			args: args{
+				p1: pointField{
+					X: big.NewInt(3),
+					Y: big.NewInt(6),
+				},
+				p2: pointField{
+					X: big.NewInt(3),
+					Y: big.NewInt(6),
+				},
+			},
+			want: pointField{
+				X: big.NewInt(80),
+				Y: big.NewInt(10),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ec := ellipticCurveField{
+				a: tt.fields.a,
+				b: tt.fields.b,
+				p: tt.fields.p,
+			}
+			if got := ec.Sum(tt.args.p1, tt.args.p2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Sum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_ellipticCurveField_Mul(t *testing.T) {
+	type fields struct {
+		a *big.Int
+		b *big.Int
+		p *big.Int
+	}
+	type args struct {
+		p1 pointField
+		n  int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   pointField
+	}{
+		{
+
+			name: "check simple sum",
+			fields: fields{
+				a: big.NewInt(2),
+				b: big.NewInt(3),
+				p: big.NewInt(97),
+			},
+			args: args{
+				p1: pointField{
+					X: big.NewInt(3),
+					Y: big.NewInt(6),
+				},
+				n: 6,
+			},
+			want: pointField{
+				X: big.NewInt(3),
+				Y: big.NewInt(6),
+			},
+		},
+		{
+
+			name: "check cofactor",
+			fields: fields{
+				a: big.NewInt(-1),
+				b: big.NewInt(3),
+				p: big.NewInt(37),
+			},
+			args: args{
+				p1: pointField{
+					X: big.NewInt(2),
+					Y: big.NewInt(3),
+				},
+				n: 6,
+			},
+			want: pointField{
+				X: big.NewInt(2),
+				Y: big.NewInt(34),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ec := ellipticCurveField{
+				a: tt.fields.a,
+				b: tt.fields.b,
+				p: tt.fields.p,
+			}
+			if got := ec.Mul(tt.args.p1, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Mul() = %v, want %v", got, tt.want)
 			}
 		})
 	}
